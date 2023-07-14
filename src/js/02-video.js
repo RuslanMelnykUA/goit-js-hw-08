09aa8fea7000dd8a0de118af201acd90fc960f9d
RuslanMelnykUA <indigo211212@gmail.com>
2023-07-14 13:25:34 +0300

    up 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,20 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-const ifFrameRef = document.querySelector('#vimeo-player');
+const ifFrameRef = document.querySelector('ifFrameRef');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 const player = new Player(ifFrameRef);
+
 player.on('timeupdate', throttle(setWatchingTime, 1000));
-function setWatchingTime(e) {
-  localStorage.setItem('videoplayer-current-time', e.seconds);
+function setWatchingTime({ seconds }) {
+  try {
+    return localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+  } catch (error) {
+    console.log(error.name);
+    return [];
+  }
 }
-const getContentOfLocalStorage =
-  localStorage.getItem('videoplayer-current-time') || 0;
-player.setCurrentTime(getContentOfLocalStorage);
+
+if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+}
